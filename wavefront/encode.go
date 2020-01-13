7ed60571adfa49bcbf5d8aed17026a0d16ed3306
ryan <26235793+rbxb@users.noreply.github.com>
2020-01-12 18:16:46 -0600

    Added comments to wavefront

--- wavefront/encode.go
+++ wavefront/encode.go
@@ -1,45 +1,48 @@
+// Package wavefront is used to encode a 3D model into the wavefront (.obj) format.
 package wavefront
 
 import (
 	"io"
-	"github.com/rbxb/smeckle/model"
 	"strconv"
+
+	"github.com/rbxb/smeckle/model"
 )
 
 func f32toA(f float32) string {
 	return strconv.FormatFloat(float64(f), 'f', -1, 32)
 }
 
-func Encode(m * model.Model, w io.Writer) error {
-	for i := 0; i < len(m.Verts) / 3; i++ {
-		w.Write([]byte("v " + 
-			f32toA(m.Verts[i*3+0]) + " " + 
-			f32toA(m.Verts[i*3+1]) + " " + 
-			f32toA(m.Verts[i*3+2]) + " \r\n"))
+// Encode encodes a 3D model in wavefront format.
+func Encode(model *model.Model, w io.Writer) {
+	for i := 0; i < len(model.Verts)/3; i++ {
+		w.Write([]byte("v " +
+			f32toA(model.Verts[i*3+0]) + " " +
+			f32toA(model.Verts[i*3+1]) + " " +
+			f32toA(model.Verts[i*3+2]) + " \r\n"))
 	}
 
 	w.Write([]byte("\r\n"))
 
-	for i := 0; i < len(m.Norms) / 3; i++ {
-		w.Write([]byte("vn " + 
-			f32toA(m.Norms[i*3+0]) + " " + 
-			f32toA(m.Norms[i*3+1]) + " " + 
-			f32toA(m.Norms[i*3+2]) + " \r\n"))
+	for i := 0; i < len(model.Norms)/3; i++ {
+		w.Write([]byte("vn " +
+			f32toA(model.Norms[i*3+0]) + " " +
+			f32toA(model.Norms[i*3+1]) + " " +
+			f32toA(model.Norms[i*3+2]) + " \r\n"))
 	}
 
 	w.Write([]byte("\r\n"))
 
-	for i := 0; i < len(m.UVs) / 2; i++ {
-		w.Write([]byte("vt " + 
-			f32toA(m.UVs[i*2+0]) + " " + 
-			f32toA(m.UVs[i*2+1]) + " \r\n"))
+	for i := 0; i < len(model.UVs)/2; i++ {
+		w.Write([]byte("vt " +
+			f32toA(model.UVs[i*2+0]) + " " +
+			f32toA(model.UVs[i*2+1]) + " \r\n"))
 	}
 
 	w.Write([]byte("\r\n"))
 
-	for i, object := range m.Objects {
+	for i, object := range model.Objects {
 		w.Write([]byte("g " + strconv.Itoa(i) + " \r\n"))
-		for f := 0; f < len(object.Faces) / 3; f++ {
+		for f := 0; f < len(object.Faces)/3; f++ {
 			v := strconv.Itoa(object.Faces[f*3+0] + 1)
 			w.Write([]byte("f " + v + "/" + v + "/" + v))
 			v = strconv.Itoa(object.Faces[f*3+1] + 1)
@@ -48,6 +51,4 @@ func Encode(m * model.Model, w io.Writer) error {
 			w.Write([]byte(" " + v + "/" + v + "/" + v + " \r\n"))
 		}
 	}
-
-	return nil
 }
