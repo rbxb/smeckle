9088be8db2dbdff20c04b297013283bbf23ff266
ryan <26235793+rbxb@users.noreply.github.com>
2020-01-12 18:10:13 -0600

    Wrote better comments in rsc0/decode

--- rsc0/decode.go
+++ rsc0/decode.go
@@ -1,44 +1,69 @@
+// Package rsc0 is used to decode 3D models from Super Evil Megacorp's format.
 package rsc0
 
 import (
-	"io"
 	"bytes"
 	"encoding/binary"
-	"github.com/rbxb/smeckle/model"
+	"io"
 )
 
+// VertexChunkUsageX are bytes that define what the data in a vertex chunk is used for.
 const (
-	VERTEX_CHUNK_USAGE_POSITION = 		0x00
-	VERTEX_CHUNK_USAGE_NORMAL = 		0x01
-	VERTEX_CHUNK_USAGE_UV = 			0x05
-
-	VERTEX_CHUNK_STORED_AS_FLOAT32 = 	0x04
-	VERTEX_CHUNK_STORED_AS_UINT8 = 		0x05
+	VertexChunkUsagePosition = 0x00
+	VertexChunkUsageNormal   = 0x01
+	VertexChunkUsageUV       = 0x05
 )
 
-type datatype struct {
-	usage, unkown, storedAs byte
-	valueCount int
-	offset int64
+// VertexChunkStoredAsX are bytes that define how a number in a vertex chunk is encoded.
+const (
+	VertexChunkStoredAsFloat32 = 0x04
+	VertexChunkStoredAsUint8   = 0x05
+)
+
+// An Object stores part of a 3D model as an array of face indices.
+type Object struct {
+	Faces []int
 }
 
-func Decode(r io.ReadSeeker) (* model.Model, error) {
-	m := model.Model{}
+// A Model stores a 3D model.
+type Model struct {
+	Name              string
+	Verts, UVs, Norms []float32
+	Objects           []Object
+}
 
-	r.Seek(36,1)
-	//Skip the first 36 bytes.
-	//I don't know what these bytes are for.
+// A vectorHeader contains values needed to read a vector.
+// Each vertex in the 3D model may have multiple vectors associated with it.
+// Each vector has it's own usage, e.g. a vector may define the position of a vertex.
+type vectorHeader struct {
+	usage      byte
+	storedAs   byte
+	valueCount int
+	offset     int64
+}
 
+// Decode reads model data that is encoded in SEMC's format from a reader and puts it in a model object.
+func Decode(r io.ReadSeeker) *Model {
+	m := &Model{}
+
+	// Skip the first 36 bytes.
+	// I don't know what these bytes do.
+	r.Seek(36, 1)
+
+	// Read the number of materials used by the model.
+	// Materials are in separate files which reference the textures and shaders used by a rendered 3D model.
 	var materialCount uint16
 	if err := binary.Read(r, binary.LittleEndian, &materialCount); err != nil {
 		panic(err)
 	}
-	//Read the number of materials used by the model.
-
 	materials := make([]string, materialCount)
+
+	// Read the names of the materials.
 	for i := range materials {
 		b := make([]byte, 1)
 		buf := bytes.NewBuffer(nil)
+		// The names are separated by zero bytes.
+		// Read characters until a zero byte is reached.
 		for {
 			_, err := r.Read(b)
 			if err != nil {
@@ -51,157 +76,163 @@ func Decode(r io.ReadSeeker) (* model.Model, error) {
 		}
 		materials[i] = string(buf.Bytes())
 	}
-	//Read the material names.
 
+	// Set the name of the model to the name of the first material that is referenced.
 	m.Name = materials[0]
 
+	// Read the number of objects in the file.
+	// Each object is it's own 3D model made of a collection of faces.
 	var objectCount uint16
 	if err := binary.Read(r, binary.LittleEndian, &objectCount); err != nil {
 		panic(err)
 	}
-	//Read the number of objects in the file.
+	m.Objects = make([]Object, objectCount)
 
-	r.Seek(2,1)
-	//Skip 2 bytes.
-	//I don't know what these bytes are for.
-	//Maybe texture count?
+	// Skip 2 bytes.
+	// I don't know what these bytes are for (Maybe texture count).
+	r.Seek(2, 1)
 
-	m.Objects = make([]model.Object, objectCount)
+	// Read the number of face indices that each object has.
+	// Each face has three indices.
+	// Each index corresponds to a vertex.
 	for i := range m.Objects {
-		r.Seek(4,1)
-		//Skip 4 bytes.
-		//I don't know what these bytes are for.
+		// Skip 4 bytes.
+		// I don't know what these bytes are for.
+		r.Seek(4, 1)
 
-		var b uint32
-		if err := binary.Read(r, binary.LittleEndian, &b); err != nil {
+		var faces uint32
+		if err := binary.Read(r, binary.LittleEndian, &faces); err != nil {
 			panic(err)
 		}
-		//Read the objectIndexCount for this object.
 
-		m.Objects[i].Faces = make([]int, int(b))
-		//Make the face buffer in the object
+		// Create the face index buffer.
+		// Every three indices represents a face.
+		m.Objects[i].Faces = make([]int, int(faces))
 
-		r.Seek(63,1)
-		//Skip 63 bytes.
-		//I don't know what these bytes are for.
+		// Skip 63 bytes.
+		// I don't know what these bytes are for.
+		r.Seek(63, 1)
 	}
-	//Read out the number of face indices in each object.
 
-	r.Seek(28,1)
-	//Skip 28 bytes.
-	//I don't know what these bytes are for.
+	// Skip 28 bytes.
+	// I don't know what these bytes are for.
+	r.Seek(28, 1)
 
-	var vertCount int
-	var vertCountUint32 uint32
-	if err := binary.Read(r, binary.LittleEndian, &vertCountUint32); err != nil {
+	// Read the number of vertices in the model.
+	var vertexCount uint32
+	if err := binary.Read(r, binary.LittleEndian, &vertexCount); err != nil {
 		panic(err)
 	}
-	vertCount = int(vertCountUint32)
-	//Read the number of vertices.
+	m.Verts = make([]float32, int(vertexCount)*3)
+	m.UVs = make([]float32, int(vertexCount)*2)
+	m.Norms = make([]float32, int(vertexCount)*3)
 
-	m.Verts = 	make([]float32, vertCount * 3)
-	m.UVs = 	make([]float32, vertCount * 2)
-	m.Norms = 	make([]float32, vertCount * 3)
-	//Make the model buffers.
+	// Skip 6 bytes.
+	// I don't know what these bytes are for.
+	r.Seek(6, 1)
 
-	r.Seek(6,1)
-	//Skip 6 bytes.
-	//I don't know what these bytes are for.
-
-	var chunkSize uint32
-	if err := binary.Read(r, binary.LittleEndian, &chunkSize); err != nil {
+	// Read the size of each vertex.
+	// A vertex is made of multiple vectors.
+	var vertexSize uint32
+	if err := binary.Read(r, binary.LittleEndian, &vertexSize); err != nil {
 		panic(err)
 	}
-	//Read the vertex chunk size.
 
-	var datatypeCount uint8
-	if err := binary.Read(r, binary.LittleEndian, &datatypeCount); err != nil {
+	// Read the number of vectors used in each vertex.
+	var vectorCount uint8
+	if err := binary.Read(r, binary.LittleEndian, &vectorCount); err != nil {
 		panic(err)
 	}
-	//Read the number of datatypes per chunk.
+	vectorHeaders := make([]vectorHeader, int(vectorCount))
 
-	datatypes := make([]datatype, int(datatypeCount))
-	for i := range datatypes {
+	// Fill in the values for each vector header.
+	for i := range vectorHeaders {
 		b := make([]byte, 4)
 		if _, err := r.Read(b); err != nil {
 			panic(err)
 		}
-		datatypes[i].usage = b[0]
-		datatypes[i].unkown = b[1]
-		datatypes[i].storedAs = b[2]
-		datatypes[i].valueCount = int(uint8(b[3]))
+		vectorHeaders[i].usage = b[0]
+		// I don't know what b[1] is used for.
+		vectorHeaders[i].storedAs = b[2]
+		vectorHeaders[i].valueCount = int(uint8(b[3]))
 		var offset uint32
 		if err := binary.Read(r, binary.LittleEndian, &offset); err != nil {
 			panic(err)
 		}
-		datatypes[i].offset = int64(offset)
+		vectorHeaders[i].offset = int64(offset)
 	}
-	//Populate the datatypes.
 
-	for i := 0; i < vertCount; i++ {
-		chunkPos, _ := r.Seek(0,1)
-		for _, dt := range datatypes {
-			r.Seek(chunkPos + dt.offset,0)
-			switch dt.usage {
-			case VERTEX_CHUNK_USAGE_POSITION:
-				switch dt.storedAs {
-				case VERTEX_CHUNK_STORED_AS_FLOAT32:
+	// Read the vertices.
+	for i := 0; i < int(vertexCount); i++ {
+		vertexOffset, _ := r.Seek(0, 1)
+		for _, vector := range vectorHeaders {
+			r.Seek(vertexOffset+vector.offset, 0)
+			switch vector.usage {
+			// This vector defines the position of the vertex.
+			case VertexChunkUsagePosition:
+				switch vector.storedAs {
+				case VertexChunkStoredAsFloat32:
 					if err := binary.Read(r, binary.LittleEndian, m.Verts[i*3+0:i*3+3]); err != nil {
 						panic(err)
 					}
 				}
-			case VERTEX_CHUNK_USAGE_NORMAL:
-				switch dt.storedAs {
-				case VERTEX_CHUNK_STORED_AS_FLOAT32:
+			// This vector defines the direction of the normal of the vertex.
+			case VertexChunkUsageNormal:
+				switch vector.storedAs {
+				case VertexChunkStoredAsFloat32:
 					if err := binary.Read(r, binary.LittleEndian, m.Norms[i*3+0:i*3+3]); err != nil {
 						panic(err)
 					}
 				}
-			case VERTEX_CHUNK_USAGE_UV:
-				switch dt.storedAs {
-				case VERTEX_CHUNK_STORED_AS_FLOAT32:
+			// This vector defines the UV position of the vertex.
+			case VertexChunkUsageUV:
+				switch vector.storedAs {
+				case VertexChunkStoredAsFloat32:
 					if err := binary.Read(r, binary.LittleEndian, m.UVs[i*2+0:i*2+2]); err != nil {
 						panic(err)
 					}
 					m.UVs[i*2+1] = 1 - m.UVs[i*2+1]
-				case VERTEX_CHUNK_STORED_AS_UINT8:
+				case VertexChunkStoredAsUint8:
 					b := make([]uint8, 2)
 					if err := binary.Read(r, binary.LittleEndian, b); err != nil {
 						panic(err)
 					}
 					m.UVs[i*2+0] = float32(b[0]) / 255
-					m.UVs[i*2+1] = 1 - float32(b[1]) / 255
+					m.UVs[i*2+1] = 1 - float32(b[1])/255
 				}
 			}
 		}
-		r.Seek(chunkPos + int64(chunkSize),0)
-	}
-	//Read the vertex chunks.
 
+		// Skip to the next vertex.
+		r.Seek(vertexOffset+int64(vertexSize), 0)
+	}
+
+	// Read the face indices of each object.
+	// Each face index is a reference to a vertex.
 	for i, object := range m.Objects {
-		for f := range object.Faces {
-			if vertCount <= 255 {
+		for face := range object.Faces {
+			// The number of bytes used to store the index is dependent on the number of vertices.
+			if vertexCount <= 255 {
 				var b uint8
 				if err := binary.Read(r, binary.LittleEndian, &b); err != nil {
 					panic(err)
 				}
-				m.Objects[i].Faces[f] = int(b)
-			} else if vertCount <= 65535 {
+				m.Objects[i].Faces[face] = int(b)
+			} else if vertexCount <= 65535 {
 				var b uint16
 				if err := binary.Read(r, binary.LittleEndian, &b); err != nil {
 					panic(err)
 				}
-				m.Objects[i].Faces[f] = int(b)
+				m.Objects[i].Faces[face] = int(b)
 			} else {
 				var b uint32
 				if err := binary.Read(r, binary.LittleEndian, &b); err != nil {
 					panic(err)
 				}
-				m.Objects[i].Faces[f] = int(b)
+				m.Objects[i].Faces[face] = int(b)
 			}
 		}
 	}
-	//Read the face indices.
 
-	return &m, nil
+	return m
 }
